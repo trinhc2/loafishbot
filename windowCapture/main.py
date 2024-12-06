61fad8fd34005316a01bd06b7710b76d0db71750
Christian Trinh <trinhc2@mcmaster.ca>
2024-12-06 16:31:33 -0500

    Update bot to work with lost ark client, base bot completed

--- windowCapture/main.py
+++ windowCapture/main.py
@@ -1,31 +1,60 @@
 import cv2 as cv
 import numpy as np
 import os
+import pyautogui
+import time
+import random
+import keyboard
 from windowcapture import WindowCapture
+
+'''
+1218,710 / 1238,719 -> remote repair
+564,785 / 892 822 -> repair all
+861,615 / 952 639 -> okay button
+'''
 
 # venv is outside of this folder so this changes the working directory
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-wincap = WindowCapture('Lost Ark 2024.12.04 - 15.20.14.02.DVR.mp4')
+wincap = WindowCapture('LOST ARK (64-bit, DX11) v.3.1.2.1')
 
 partialImage = cv.imread('partial.jpg', cv.IMREAD_UNCHANGED)
+# WindowCapture.listWindows()
 
-while (True):
+botStarted = False
+fishingKey = 'e'
 
-    # get an updated image of the game
-    screenshot = wincap.getScreenshot()
-    screenshot = np.copy(screenshot)
+print("Waiting for user to begin fishing...")
 
-    result = cv.matchTemplate(screenshot, partialImage, cv.TM_CCOEFF_NORMED)
-    minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(result)
-    # cv.imshow('Computer Vision', screenshot)
-    # print(maxVal)
-    threshold = 0.9
-    if maxVal >= threshold:
-        print("found")
+while (not botStarted):
+    if keyboard.is_pressed(fishingKey):
+        print("Bot started. Press Ctrl+C to stop at any time.")
+        # botStarted = True
+    print(pyautogui.position())
 
-    # press 'q' with the output window focused to exit.
-    # waits 1 ms every loop to process key presses
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
-        break
+try:
+    while (botStarted):
+        # get an updated image of the game
+        screenshot = wincap.getScreenshot()
+        screenshot = np.copy(screenshot)
+
+        result = cv.matchTemplate(
+            screenshot, partialImage, cv.TM_CCOEFF_NORMED)
+        minVal, maxMatchValue, minLoc, maxLoc = cv.minMaxLoc(result)
+        cv.imshow('Computer Vision', screenshot)
+        # print(maxVal)
+        matchingThreshold = 0.9
+        if maxMatchValue >= matchingThreshold:
+            print("found")
+            pyautogui.press(fishingKey)
+            time.sleep(random.uniform(6, 7.5))
+            pyautogui.press(fishingKey)
+
+        # press 'q' with the output window focused to exit.
+        # waits 1 ms every loop to process key presses
+        if cv.waitKey(1) == ord('q'):
+            cv.destroyAllWindows()
+            break
+
+except KeyboardInterrupt:
+    print("Bot stopped.")
